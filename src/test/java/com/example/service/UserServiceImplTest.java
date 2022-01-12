7e596441ffc09bc639d4c38ef4bed870df6b02bd
Hódi Szilárd <hodiszilard89@gmail.com>
2022-01-12 15:41:46 +0100

    loginúj

--- src/test/java/com/example/service/UserServiceImplTest.java
+++ src/test/java/com/example/service/UserServiceImplTest.java
@@ -7,12 +7,14 @@ import com.example.hunyerft.service.UserService;
 import com.example.hunyerft.service.UserServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.Assertion;
 
 import java.util.Optional;
 
@@ -23,16 +25,19 @@ public class UserServiceImplTest {
     @InjectMocks
     private UserServiceImpl underTest;
     private UserRepository userRepository;
+    private Vizsga testVizsga;
     private String TEST_USER_NEV="testszilard";
+    private User user;
 
     @Before
     public void init(){
          userRepository= Mockito.mock(UserRepository.class);
+         user = Mockito.mock(User.class);
+         testVizsga = Mockito.mock(Vizsga.class);
          underTest=new UserServiceImpl(userRepository);
+        // user = createUser();
     }
 
-    @Mock
-    private Vizsga testVizsga;
 
     @AfterMethod
     public void tearDown() {
@@ -43,12 +48,18 @@ public class UserServiceImplTest {
     public void testUserPickUpExamThenNumberOfExamIsOneMore(){
 
         //give
-        User exceptid = createUser();
         given(userRepository.findByNev(TEST_USER_NEV)).willReturn(createUser());
         //when
         //then
     }
 
+
+    @Test
+    public void testGetVizsgak(){
+        testVizsga=createVizsga();
+        user.addVizsga(testVizsga);
+        Assertions.assertNotEquals(user.getVizsgak(),null);
+    }
 
     private User createUser(){
         User user = new User();
@@ -60,6 +71,16 @@ public class UserServiceImplTest {
         return user;
     }
 
+    private Vizsga createVizsga(){
+        Vizsga vizsga = new Vizsga();
+        vizsga.setId(1);
+        vizsga.setVizsgaNev("Zh1");
+        vizsga.setHelySzin("Nyíregyháza");
+        vizsga.setDatum("2022-01-12");
+        vizsga.setIdoPont("12:30");
+        vizsga.setTargyNev("Analizis");
+        return vizsga;
+    }
     private Optional<User> createOptionalUser(){
         return Optional.of(createUser());
     }
